b6f771229f1c707a5bcb76182f0bcd59c3c52d04
beataj <beata@emc2wave.com>
2021-06-14 16:51:59 -0700

    add btn to servers.component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,8 +4,9 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-selector',
-  template: `<app-server></app-server>
-  <app-server></app-server>`,
+  // template: `<app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
